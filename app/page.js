6a63039f09221a3561aa6a1644ccbf65dc755f76
Richard Adler <richard.adler92@gmail.com>
2024-09-10 18:59:35 +0100

    Hotjar fixed

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 "use client"; 
 
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Navbar from '@/components/global/Navbar';
 import HomeHeader from '@/components/page-specific/Home/HomeHeader';
 import SearchForm from '@/components/page-specific/Home/SearchForm';
