b03558ca83ac769be3acbffb0dfc0a2bd6f10ad4
Richard Adler <richard.adler92@gmail.com>
2024-09-14 21:17:41 +0100

    FAQ Header fixed

--- app/faq/page.js
+++ app/faq/page.js
@@ -68,7 +68,7 @@ const FAQ = () => {
       <header className="relative h-[45vh] sm:h-[45vh] md:h-[45vh] lg:h-[45vh]">
         <div
           className="absolute inset-0 w-full h-full bg-cover bg-center"
-          style={{ backgroundImage: `url('/images/header3.jpg')` }}
+          style={{ backgroundImage: `url('/images/Header3.jpg')` }}
         />
         <div className="absolute inset-0 bg-black bg-opacity-50 flex flex-col justify-center items-center text-white p-4">
           <h1 className="text-5xl font-bold mb-4 drop-shadow-lg">
