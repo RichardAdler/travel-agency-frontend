34ac9c6083fd6782937f9f33aab625abe5237d9f
Richard Adler <richard.adler92@gmail.com>
2024-09-14 21:39:10 +0100

    Hotelcard updated.

--- app/components/global/HotelCard.js
+++ app/components/global/HotelCard.js
@@ -2,9 +2,10 @@ import Link from 'next/link';
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
-const HotelCard = ({ hotel, checkInDate, nights, adults }) => { // Receive parameters as props
+const HotelCard = ({ hotel, checkInDate, nights, adults }) => {
     const [photoUrl, setPhotoUrl] = useState('');
     const backendUrl = process.env.NEXT_PUBLIC_BACKEND_URL;
+
     useEffect(() => {
         const fetchPhotos = async () => {
             try {
@@ -25,17 +26,21 @@ const HotelCard = ({ hotel, checkInDate, nights, adults }) => { // Receive param
     }, [backendUrl, hotel.location_id]);
 
     return (
-        <div className="max-w-xs rounded-md overflow-hidden shadow-lg bg-white flex flex-col">
-            <img className="w-full h-48 object-cover" src={photoUrl || "/images/default-hotel.jpg"} alt={hotel.name} />
-            <div className="flex flex-col flex-grow justify-between p-6">
+        <div className="max-w-xs md:max-w-sm lg:max-w-md rounded-md overflow-hidden shadow-lg bg-white flex flex-col transition-transform hover:scale-105">
+            <img 
+                className="w-full h-48 md:h-60 lg:h-72 object-cover" 
+                src={photoUrl || "/images/default-hotel.jpg"} 
+                alt={hotel.name} 
+            />
+            <div className="flex flex-col flex-grow justify-between p-4 md:p-6">
                 <div>
-                    <div className="font-bold text-xl mb-2">{hotel.name}</div>
-                    <p className="text-gray-700 text-base">{hotel.address_obj.address_string}</p>
+                    <div className="font-bold text-lg md:text-xl lg:text-2xl mb-2">{hotel.name}</div>
+                    <p className="text-gray-700 text-sm md:text-base">{hotel.address_obj.address_string}</p>
                 </div>
                 <div className="flex justify-center mt-4">
                     {/* Pass query parameters in the URL */}
                     <Link href={`/location/${hotel.location_id}?checkInDate=${checkInDate}&nights=${nights}&adults=${adults}`}>
-                        <button className="bg-[#E16A3D] hover:bg-[#FFA450] text-white font-bold py-2 px-4 rounded-md">
+                        <button className="bg-[#E16A3D] hover:bg-[#FFA450] text-white font-bold py-2 px-4 rounded-md transition-colors duration-300">
                             View Details
                         </button>
                     </Link>
